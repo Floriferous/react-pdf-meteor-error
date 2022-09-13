49cf3a5cbca0c712112f8f544986de70abd56e00
Florian Bienefelt <florian.bienefelt@gmail.com>
2022-09-13 16:30:04 +0200

    Update package versions

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,12 @@
 import { App } from '/imports/ui/App';
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
   root.render(App());
+  // render(<App />, document.getElementById('react-target'));
 });
